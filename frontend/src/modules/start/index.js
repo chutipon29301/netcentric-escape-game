8fe549e303b4399e2951e0bf48e1a83a0a2ede4f
Jatuwatsag <thehandsomeify@gmail.com>
2018-10-23 14:34:15 +0700

    refactor(front->start): change UI of start page

--- frontend/src/modules/start/index.js
+++ frontend/src/modules/start/index.js
@@ -1,60 +1,60 @@
-import React, { Component } from 'react';
-import './style.scss';
-import { inject, observer } from 'mobx-react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import "./style.scss";
+import { inject, observer } from "mobx-react";
+import { withRouter } from "react-router-dom";
 
-@inject('routing')
+@inject("routing")
 @withRouter
 @observer
 class Start extends Component {
-	handleStart() {
-		this.props.routing.push('/login');
-	}
+  handleStart() {
+    this.props.routing.push("/login");
+  }
 
-	render() {
-		return (
-			<div className="start">
-				<div className="container">
-					<div className="row">
-						<div className="col">
-							<div className="gameLogo">
-								<img
-									src="http://breakout.gamemiles.com/content/images/thumbs/0161767_580.jpeg"
-									alt=""
-								/>
-							</div>
-						</div>
-						<div className="col">
-							<div className="stater">
-								<h2>Netcentric Escrape Game</h2>
-								<hr />
-								<p>
-									Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit, sed do eiusmod tempor
-									incididunt ut labore et dolore magna aliqua.
-									Ut enim ad minim veniam, quis nostrud
-									exercitation ullamco laboris nisi ut aliquip
-									ex ea commodo consequat. Duis aute irure
-									dolor in reprehenderit in voluptate velit
-									esse cillum dolore eu fugiat nulla pariatur.
-									Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt
-									mollit anim id est laborum.
-								</p>
-							</div>
-							<button
-								type="button"
-								className="btn btn-primary btn-lg round raised"
-								onClick={() => this.handleStart()}
-							>
-								Start!
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="start">
+        <div className="container h-100">
+          <div className="row h-100">
+            <div className="col-lg-7 my-auto">
+              <div className="header-content mx-auto">
+                <h1 className="mb-5">
+                  Netcentric Escape Socket Game
+                </h1>
+				<p className="mb-5">
+                  New Age is an web landing page that will help you beautifully
+                  showcase your new mobile web app, or anything else!
+                </p>
+                <a
+                  href="#download"
+				  className="btn btn-outline btn-xl js-scroll-trigger"
+				  onClick={()=>this.handleStart()}
+                >
+                  Start Now for Free!
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-5 my-auto">
+              <div className="device-container">
+                <div className="device-mockup iphone6_plus portrait white">
+                  <div className="device">
+                    <div className="screen">
+                      <img
+                        src="http://breakout.gamemiles.com/content/images/thumbs/0161767_580.jpeg"
+                        className="img-fluid"
+                        alt=""
+                      />
+                    </div>
+                    <div className="button" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Start;
