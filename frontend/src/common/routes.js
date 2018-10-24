3d59ced83b9dcef713d1161c160d2d978e745040
Jatuwatsag <thehandsomeify@gmail.com>
2018-10-25 02:05:38 +0700

    feat(front->route): adding waitingRoom route

--- frontend/src/common/routes.js
+++ frontend/src/common/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route, Router } from 'react-router-dom';
 import Start from '../modules/start';
 import Login from '../modules/login';
 import Register from '../modules/register';
+import WaitingRoom from '../modules/waitingRoom';
 
 
 class AppRouter extends React.Component {
@@ -13,6 +14,7 @@ class AppRouter extends React.Component {
 					<Route exact path="/" component={Start} />
 					<Route path="/login" component={Login} />
 					<Route path="/register" component={Register} />
+					<Route path="/waiting" component={WaitingRoom}/>
 				</Switch>
 			</Router>
 		);
