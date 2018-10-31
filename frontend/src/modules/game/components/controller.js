2da3a6aff0cf1df31b30a74af37e07725a83c11a
Jatuwatsag <thehandsomeify@gmail.com>
2018-10-31 13:14:08 +0700

    feat(front->game): add new game feat

--- frontend/src/modules/game/components/controller.js
+++ frontend/src/modules/game/components/controller.js
@@ -10,15 +10,19 @@ export default class Controller extends React.Component {
       <div className="controller-container">
         <div className="container">
           <div>
+            <h3>
+              Game Controller
+            </h3>
             <p>
               You are : <b>{this.props.game.player.type}</b>
             </p>
           </div>
           <div className="button">
-            <div class="row">
-              <div class="col align-self-start">1</div>
-              <div class="col align-self-center">2</div>
-              <div class="col align-self-end">3</div>
+            <div className="row">
+              <span className="start-btn keyboard_key_up" onClick={()=>this.props.game.walkUp()}>Up</span>
+              <span className="start-btn keyboard_key_left" onClick={()=>this.props.game.walkLeft()}>Left</span>
+              <span className="start-btn keyboard_key_down" onClick={()=>this.props.game.walkDown()}>Down</span>
+              <span className="start-btn keyboard_key_right" onClick={()=>this.props.game.walkRight()}>Right</span>
             </div>
           </div>
         </div>
--- frontend/src/modules/game/components/field.js
+++ frontend/src/modules/game/components/field.js
@@ -2,35 +2,31 @@ import React, { Component } from "react";
 import "./fieldStyle.scss";
 import { inject, observer } from "mobx-react";
 
-// function createTable(row, col) {
-//   const x = [];
-//   for (let i = 0; i < row; i++) {
-//     const temp = [];
-//     for (let j = 0; j < col; j++) {
-//       temp.push("");
-//     }
-//     x.push(temp);
-//   }
-//   return x;
-// }
 @inject("game")
 @observer
 export default class Field extends React.Component {
-
   render() {
     return (
       <div className="table-field">
-        
         <table>
-          {this.props.game.createTable(5, 5).map(row => {
-            return (
-              <tr>
-                {row.map(() => {
-                  return <td />;
-                })}
-              </tr>
-            );
-          })}
+          <tbody>
+            {this.props.game.createTable(this.props.game.player).map(obj => {
+              return (
+                <tr key={obj.x}>
+                  {obj.map(
+                    data =>
+                      data === "blank" ? (
+                        <td key={data.x} />
+                      ) : (
+                        <td key={data.y}>
+                          <img src={data} />
+                        </td>
+                      )
+                  )}
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     );
