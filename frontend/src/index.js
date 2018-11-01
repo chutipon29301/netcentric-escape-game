8f49b43f1b325694d2e5bc8c1a6a361837d16dec
BB-8 <ichannel.jakpat@gmail.com>
2018-11-01 17:15:15 +0700

    feat(front): unregister serviceworker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import { configure } from "mobx";
-import { Provider } from "mobx-react";
-import Router from "./common/routes";
-import registerServiceWorker from "./registerServiceWorker";
-import { RouterStore, syncHistoryWithStore } from "mobx-react-router";
-import createBrowserHistory from "history/createBrowserHistory";
-import start from "modules/start/store";
-import login from "modules/login/store";
-import register from "modules/register/store";
-import { roomStore } from "modules/waitingRoom/store";
-import game from "modules/game/store";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure } from 'mobx';
+import { Provider } from 'mobx-react';
+import Router from './common/routes';
+import * as registerServiceWorker from './registerServiceWorker';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import start from 'modules/start/store';
+import login from 'modules/login/store';
+import register from 'modules/register/store';
+import waitingRoom from 'modules/waitingRoom/store';
+import game from 'modules/game/store';
+
 
 const browserHistory = createBrowserHistory();
 export const routingStore = new RouterStore();
@@ -34,4 +35,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
+registerServiceWorker.unregister();
