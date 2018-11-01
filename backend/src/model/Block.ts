94b0e1ba08db31c12c216ea6b5ab2496a540fa7f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-11-02 00:22:58 +0700

    refactor(back): Delete unused files

--- backend/src/model/Block.ts
+++
@@ -1,8 +0,0 @@
-import { BlockType } from "../type/blockType";
-import { Coordinate } from "./Coordinate";
-
-export class Block {
-
-    constructor(public blockType: BlockType, public coordinate: Coordinate) { }
-
-}
--- backend/src/model/Coordinate.ts
+++
@@ -1,8 +0,0 @@
-export class Coordinate {
-
-    constructor(private x: number, private y: number) { }
-
-    public equal(that: Coordinate): boolean {
-        return this.x === that.x && this.y === that.y;
-    }
-}
