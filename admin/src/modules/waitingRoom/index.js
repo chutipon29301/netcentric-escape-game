27922674a0b13b42f70324f53f87bd29253834ca
Jatuwatsag <thehandsomeify@gmail.com>
2018-11-02 10:43:47 +0700

    commit waiting and config

--- admin/src/modules/waitingRoom/index.js
+++ admin/src/modules/waitingRoom/index.js
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from 'react';
 import WaitingUser from './components/waitingUser';
-import LoginUser from './components/loginUser'
-import Rooms from './components/rooms'
-import UserInRoom from './components/userInRoom'
+import LoginUser from './components/loginUser';
+import Rooms from './components/rooms';
+import UserInRoom from './components/userInRoom';
 export default class WaitingRoom extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="d-flex flex-row">
-                    <div className="col-4">
-                        <h2 className="mt-1">Room</h2>
-                        <LoginUser />
-                    </div>
-                    <div className="col-8">
-                        <WaitingUser />
-                    </div>
-                </div>
-                
-                    <Rooms />
-                
-            </div>
+	render() {
+		return (
+			<div>
+				<div className="d-flex flex-row">
+					<div className="col-4">
+						<h2 className="mt-1">Room</h2>
+						<LoginUser />
+					</div>
+					<div className="col-8">
+						<WaitingUser />
+					</div>
+				</div>
 
-        )
-    }
+				<Rooms />
+			</div>
+		);
+	}
 }
--- frontend/src/modules/waitingRoom/store.js
+++ frontend/src/modules/waitingRoom/store.js
@@ -1,77 +1,47 @@
-import { observable, action } from 'mobx';
-import loginStore from '../login/store'
-
-class LobbyStore {
-	@observable
-	rooms = [{
-		roomToken: '0', name: 'Room1', number: 1
-	},
-	{
-		roomToken: '1', name: 'Room2', number: 2
-	},
-	{
-		roomToken: '2', name: 'Room3', number: 2
-	},
-	{
-		roomToken: '3', name: 'Room4', number: 1
-	},
-	{
-		roomToken: '4', name: 'Room5', number: 9
-	}];
-
-	@action.bound
-	setRooms(rooms) {
-		this.rooms = rooms;
-	}
-}
+import { observable, action } from "mobx";
 
 class RoomStore {
-	@observable
-	room = {
-		roomToken: '0', name: 'Example', players: [
-			{ playerToken: '9999', name: 'Player0', isReady: false },
-			{ playerToken: '4534', name: 'Player1', isReady: true },
-			{ playerToken: '7659', name: 'Player2', isReady: false }]
-	};
+  @observable
+  room = {
+    roomToken: "0",
+    name: "Room",
+    players: [
+      { playerToken: "9999", name: "Player0", isReady: false },
+      { playerToken: "4534", name: "Player1", isReady: true },
+      { playerToken: "7659", name: "Player2", isReady: false }
+    ]
+  };
 
+  @action.bound
+  setRoom(room) {
+    this.room = room;
+  }
 
-	@action.bound
-	setRoom(room) {
-		this.room = room;
-	}
+  @action.bound
+  getEveryoneReady() {
+    for (var player in this.room.players) {
+      if (!player.isReady) {
+        return false;
+      }
+    }
 
-	@action.bound
-	getEveryoneReady() {
-		for (var player in this.room.players) {
-			if (!player.isReady) {
-				return false;
-			}
-		}
+    return true;
+  }
 
-		return true;
-	}
+  @action.bound
+  getMyReady() {
+    return this.room.players.find(e => e.playerToken === "9999").isReady;
+  }
 
-	@action.bound
-	getMyReady() {
-		return this.room.players.find(e => e.playerToken === '9999').isReady;
-	}
-
-	// When ready is clicked, playerToken and isReady is sent to server, 
-	// and server sends back room info again
-	@action.bound
-	setMyReady(myReady) {
-		const newRoom = {...this.room};
-		newRoom.players.find(e => e.playerToken === '9999').isReady = myReady;
-		this.room = newRoom;
-	}
+  // When ready is clicked, playerToken and isReady is sent to server,
+  // and server sends back room info again
+  @action.bound
+  setMyReady(myReady) {
+    const newRoom = { ...this.room };
+    newRoom.players.find(e => e.playerToken === "9999").isReady = myReady;
+    this.room = newRoom;
+  }
 }
 
-export const lobbyStore = new LobbyStore();
 export const roomStore = new RoomStore();
 
-class WaitingRoomStore {
-	@observable 
-	myToken = loginStore.token;
-}
-
-export const waitingRoomStore = new WaitingRoomStore();
