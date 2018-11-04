739d9920a01f5f26cc90c68b5115d2bcb903d7e3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-11-04 15:49:40 +0700

    feat(back->game): Create game listener socket

--- backend/src/controllers/socket/game.ts
+++ backend/src/controllers/socket/game.ts
@@ -3,6 +3,7 @@ import url from "url";
 import WebSocket from "ws";
 import { GameArray } from "../../model/game/GameArray";
 import { GameSocket as Socket } from "../../model/game/GameSocket";
+import { Socket as ObservableSocket } from "../../model/socket/Socket";
 import { SocketGenerator } from "../../model/socket/SocketGenerator";
 import { JWTAuth } from "../../repositories/JWTAuth";
 
@@ -18,22 +19,26 @@ export class GameSocket {
     private static instance: GameSocket;
 
     private webSocketServer = SocketGenerator.getInstance().createSocket("/game",
-    (info: { origin: string; secure: boolean; req: IncomingMessage }) => {
-        const { query: { token, player } } = url.parse(info.req.url, true);
-        try {
-            JWTAuth.decodeToken(player as string);
-            return GameArray.getInstance().checkGameExist(token as string);
-        } catch (_) {
-            return false;
-        }
-    });
+        (info: { origin: string; secure: boolean; req: IncomingMessage }) => {
+            const { query: { token, player } } = url.parse(info.req.url, true);
+            try {
+                JWTAuth.decodeToken(player as string);
+                return GameArray.getInstance().checkGameExist(token as string);
+            } catch (_) {
+                return false;
+            }
+        });
+    private webSocketServerListener = SocketGenerator.getInstance().createSocket("/gameListener");
 
     public init() {
         this.webSocketServer.on("connection", (socket: WebSocket, req: IncomingMessage) => {
             const { query: { token, player } } = url.parse(req.url, true);
             const game = GameArray.getInstance().getGameWithToken(token as string);
             const observableSocket = new Socket(socket, player as string);
-            // game.getSubject().subscribe((message) => observableSocket.send(message));
+        });
+
+        this.webSocketServerListener.on("connection", (socket: WebSocket) => {
+            const observableSocket = new ObservableSocket<{}, {}>(socket);
         });
     }
 }
