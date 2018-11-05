79fac8a58638442b3414dce0b5a4dbf02edefcc8
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-11-05 11:04:21 +0700

    feat(front->env): Set new environment setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure } from 'mobx';
-import { Provider } from 'mobx-react';
-import Router from './common/routes';
-import * as registerServiceWorker from './registerServiceWorker';
-import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
-import createBrowserHistory from 'history/createBrowserHistory';
-import start from 'modules/start/store';
-import login from 'modules/login/store';
-import register from 'modules/register/store';
-import { roomStore } from 'modules/waitingRoom/store';
-import game from 'modules/game/store';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure } from "mobx";
+import { Provider } from "mobx-react";
+import Router from "./common/routes";
+import * as registerServiceWorker from "./registerServiceWorker";
+import { RouterStore, syncHistoryWithStore } from "mobx-react-router";
+import createBrowserHistory from "history/createBrowserHistory";
 
 
 const browserHistory = createBrowserHistory();
 export const routingStore = new RouterStore();
 const history = syncHistoryWithStore(browserHistory, routingStore);
 const stores = {
-  start,
-  login,
-  register,
-  roomStore,
-  game,
   routing: routingStore
 };
 
