72990f79be2d9bba82d8064c4fa8737e8f1063c4
BB-8 <ichannel.jakpat@gmail.com>
2018-11-08 23:54:11 +0700

    fix(admin): missing import `.env`

--- admin/src/env.js
+++ admin/src/env.js
@@ -1,10 +1,10 @@
 export let BASE_URL;
-export let WEBSOCKET_URL;
+export let SOCKET_URL;
 
 if (process.env.NODE_ENV === 'production') {
 	BASE_URL = `https://${window.location.hostname}/api`;
-	WEBSOCKET_URL = `wss://${window.location.hostname}/api`;
+	SOCKET_URL = `wss://${window.location.hostname}/api`;
 } else {
 	BASE_URL = 'http://localhost:8000/api';
-	WEBSOCKET_URL = 'ws://localhost:8000/api';
+	SOCKET_URL = 'ws://localhost:8000/api';
 }
--- admin/src/modules/editUser/currentUser.js
+++ admin/src/modules/editUser/currentUser.js
@@ -1,96 +1,107 @@
-import React from 'react'
-import Axios from '../axiosConfig'
-import {SOCKET_URL} from '../../env'
+import React from 'react';
+import Axios from '../axiosConfig';
+import { SOCKET_URL } from '../../env';
 
 class CurrentUser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tableData: [],
-        };
-        
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			tableData: []
+		};
+	}
 
-    deletePost(user) {
-        Axios({
-            method: 'delete',
-            url: '/user',
-            data: user
-        }).then((response) => {})
-        .catch((err) => console.log(err));
-    }
+	deletePost(user) {
+		Axios({
+			method: 'delete',
+			url: '/user',
+			data: user
+		})
+			.then(response => {})
+			.catch(err => console.log(err));
+	}
 
-    componentDidMount() {
-        this.connectSocket();
-    }
-    resetScore(){
-        Axios({
-            method: 'post',
-            url: '/resetScore',
-            data: this.state
-        }).then((response) => {});
-    }
+	componentDidMount() {
+		this.connectSocket();
+	}
+	resetScore() {
+		Axios({
+			method: 'post',
+			url: '/resetScore',
+			data: this.state
+		}).then(response => {});
+	}
 
-    connectSocket(){
-        let socket = new WebSocket(`${SOCKET_URL}/player`);
+	connectSocket() {
+		let socket = new WebSocket(`${SOCKET_URL}/player`);
 
-        socket.addEventListener("message", (event) => {
-            let tableData = [];
-            try{
-                tableData = JSON.parse(event.data);
-                this.setState({ tableData  });
-                console.log(tableData)
-            }catch(error){
+		socket.addEventListener('message', event => {
+			let tableData = [];
+			try {
+				tableData = JSON.parse(event.data);
+				this.setState({ tableData });
+				console.log(tableData);
+			} catch (error) {}
+		});
+		socket.addEventListener('error', error => {
+			console.log(error);
+		});
+		socket.addEventListener('close', event => {
+			if (event.code === 1006) {
+				console.log('reconnect');
+				this.connectSocket();
+			}
+		});
+	}
 
-            }
-        });
-        socket.addEventListener("error",(error) => {
-            console.log(error);
-        });
-        socket.addEventListener("close",(event) => {
-            if(event.code === 1006){
-                console.log("reconnect")
-                this.connectSocket();
-            }
-        });
-    }
-
-
-    render() {
-        return (
-            <div>
-                <table className="table mt-5">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nickname</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Win</th>
-                            <th scope="col">Edit</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.tableData.map(function (row, index) {
-                                return <tr key={index} >
-                                    <td>{index + 1}</td>
-                                    <td>{row.nickname}</td>
-                                    <td>{row.email}</td>
-                                    <td>{row.win}</td>
-                                    <td><button name="delete" onClick={()=>this.deletePost(row)} className="btn btn-outline-danger btn-sm remove">Delete</button></td>
-                                </tr>
-                            }.bind(this))
-                        }
-                    </tbody>
-                </table>
-                <button className="delete" onClick={()=>this.resetScore()} className="btn btn-outline-info btn-sm remove">Reset Score!</button>
-                
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<table className="table mt-5">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">Nickname</th>
+							<th scope="col">Email</th>
+							<th scope="col">Win</th>
+							<th scope="col">Edit</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.tableData.map(
+							function(row, index) {
+								return (
+									<tr key={index}>
+										<td>{index + 1}</td>
+										<td>{row.nickname}</td>
+										<td>{row.email}</td>
+										<td>{row.win}</td>
+										<td>
+											<button
+												name="delete"
+												onClick={() =>
+													this.deletePost(row)
+												}
+												className="btn btn-outline-danger btn-sm remove"
+											>
+												Delete
+											</button>
+										</td>
+									</tr>
+								);
+							}.bind(this)
+						)}
+					</tbody>
+				</table>
+				<button
+					className="delete"
+					onClick={() => this.resetScore()}
+					className="btn btn-outline-info btn-sm remove"
+				>
+					Reset Score!
+				</button>
+			</div>
+		);
+	}
 }
 
 export default CurrentUser;
-
-
